ea48ab17273a63e8569b9a0042fd7903ea13b7e9
Harrison English <henglish3@gatech.edu>
2011-10-03 17:12:07 -0400

    added current rations and current pace to status screen

--- src/views/CharStatusInterface.java
+++ src/views/CharStatusInterface.java
@@ -46,13 +46,13 @@ public class CharStatusInterface {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JLabel lblName = new JLabel("Name");
-		lblName.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+		lblName.setFont(new Font("Lucida Grande", Font.BOLD, 16));
 		
 		JLabel lblStatus = new JLabel("Status");
-		lblStatus.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+		lblStatus.setFont(new Font("Lucida Grande", Font.BOLD, 16));
 		
 		JLabel lblProfession = new JLabel("Profession");
-		lblProfession.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+		lblProfession.setFont(new Font("Lucida Grande", Font.BOLD, 16));
 		
 		JLabel lblMainCharName = new JLabel("Harrison");
 		
@@ -64,7 +64,7 @@ public class CharStatusInterface {
 		lblMainCharPic.setIcon(new ImageIcon("/Users/harrisonenglish/Desktop/face.png"));
 		
 		JLabel lblAge = new JLabel("Age");
-		lblAge.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
+		lblAge.setFont(new Font("Lucida Grande", Font.BOLD, 16));
 		
 		JLabel lblMainCharAge = new JLabel("24");
 		
@@ -103,6 +103,17 @@ public class CharStatusInterface {
 		
 		JButton btnDone = new JButton("Done");
 		
+		JLabel lblPace = new JLabel("Pace");
+		lblPace.setFont(new Font("Lucida Grande", Font.BOLD, 15));
+		
+		JLabel lblCurrentPace = new JLabel("Normal");
+		lblCurrentPace.setFont(new Font("Lucida Grande", Font.PLAIN, 12));
+		
+		JLabel lblRations = new JLabel("Rations");
+		lblRations.setFont(new Font("Lucida Grande", Font.BOLD, 15));
+		
+		JLabel lblCurrentRations = new JLabel("Enough");
+		
 		
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
 		groupLayout.setHorizontalGroup(
@@ -110,12 +121,12 @@ public class CharStatusInterface {
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblMainCharPic, GroupLayout.PREFERRED_SIZE, 84, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblChar2Pic, GroupLayout.PREFERRED_SIZE, 84, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblChar3Pic, GroupLayout.PREFERRED_SIZE, 84, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblChar4Pic, GroupLayout.PREFERRED_SIZE, 84, GroupLayout.PREFERRED_SIZE))
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addGroup(groupLayout.createSequentialGroup()
-							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-								.addComponent(lblMainCharPic, GroupLayout.PREFERRED_SIZE, 84, GroupLayout.PREFERRED_SIZE)
-								.addComponent(lblChar2Pic, GroupLayout.PREFERRED_SIZE, 84, GroupLayout.PREFERRED_SIZE)
-								.addComponent(lblChar3Pic, GroupLayout.PREFERRED_SIZE, 84, GroupLayout.PREFERRED_SIZE)
-								.addComponent(lblChar4Pic, GroupLayout.PREFERRED_SIZE, 84, GroupLayout.PREFERRED_SIZE))
 							.addGap(26)
 							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 								.addComponent(lblName)
@@ -123,7 +134,7 @@ public class CharStatusInterface {
 								.addComponent(lblChar4Name, GroupLayout.PREFERRED_SIZE, 54, GroupLayout.PREFERRED_SIZE)
 								.addComponent(lblChar3Name, GroupLayout.PREFERRED_SIZE, 54, GroupLayout.PREFERRED_SIZE)
 								.addComponent(lblChar2Name, GroupLayout.PREFERRED_SIZE, 54, GroupLayout.PREFERRED_SIZE))
-							.addPreferredGap(ComponentPlacement.RELATED, 55, Short.MAX_VALUE)
+							.addPreferredGap(ComponentPlacement.RELATED, 59, Short.MAX_VALUE)
 							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 								.addComponent(lblAge)
 								.addComponent(lblMainCharAge)
@@ -137,7 +148,7 @@ public class CharStatusInterface {
 								.addComponent(lblChar2Job, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
 								.addComponent(lblChar3Job, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
 								.addComponent(lblChar4Job, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE))
-							.addPreferredGap(ComponentPlacement.RELATED, 49, Short.MAX_VALUE)
+							.addPreferredGap(ComponentPlacement.RELATED, 54, Short.MAX_VALUE)
 							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 								.addComponent(lblMainCharStatus)
 								.addComponent(lblStatus)
@@ -145,57 +156,73 @@ public class CharStatusInterface {
 								.addComponent(lblChar3Status, GroupLayout.PREFERRED_SIZE, 26, GroupLayout.PREFERRED_SIZE)
 								.addComponent(lblChar4Status, GroupLayout.PREFERRED_SIZE, 26, GroupLayout.PREFERRED_SIZE))
 							.addGap(89))
-						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(37)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblPace)
+								.addComponent(lblCurrentPace))
+							.addGap(29)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblRations, GroupLayout.PREFERRED_SIZE, 74, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblCurrentRations))
+							.addPreferredGap(ComponentPlacement.RELATED, 234, Short.MAX_VALUE)
 							.addComponent(btnDone)
 							.addContainerGap())))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(34)
-							.addComponent(lblMainCharPic, GroupLayout.PREFERRED_SIZE, 78, GroupLayout.PREFERRED_SIZE)
-							.addGap(18)
-							.addComponent(lblChar2Pic, GroupLayout.PREFERRED_SIZE, 78, GroupLayout.PREFERRED_SIZE)
-							.addGap(18)
-							.addComponent(lblChar3Pic, GroupLayout.PREFERRED_SIZE, 78, GroupLayout.PREFERRED_SIZE)
-							.addGap(18)
-							.addComponent(lblChar4Pic, GroupLayout.PREFERRED_SIZE, 78, GroupLayout.PREFERRED_SIZE))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(19)
-							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-								.addComponent(lblName)
-								.addComponent(lblAge)
-								.addComponent(lblProfession)
-								.addComponent(lblStatus))
-							.addGap(15)
-							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-								.addComponent(lblMainCharName)
-								.addComponent(lblMainCharAge)
-								.addComponent(lblMainCharJob)
-								.addComponent(lblMainCharStatus))
-							.addGap(77)
-							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-								.addComponent(lblChar2Name)
-								.addComponent(lblChar2Age)
-								.addComponent(lblChar2Job)
-								.addComponent(lblChar2Status))
-							.addGap(79)
-							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-								.addComponent(lblChar3Name)
-								.addComponent(lblCha3Age)
-								.addComponent(lblChar3Job)
-								.addComponent(lblChar3Status))
-							.addGap(84)
-							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-								.addComponent(lblChar4Name)
-								.addComponent(lblChar4Age)
-								.addComponent(lblChar4Job)
-								.addComponent(lblChar4Status))))
+					.addGap(34)
+					.addComponent(lblMainCharPic, GroupLayout.PREFERRED_SIZE, 78, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(lblChar2Pic, GroupLayout.PREFERRED_SIZE, 78, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(lblChar3Pic, GroupLayout.PREFERRED_SIZE, 78, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(lblChar4Pic, GroupLayout.PREFERRED_SIZE, 78, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED, 7, Short.MAX_VALUE)
 					.addComponent(btnDone)
 					.addContainerGap())
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(19)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblName)
+						.addComponent(lblAge)
+						.addComponent(lblProfession)
+						.addComponent(lblStatus))
+					.addGap(15)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblMainCharName)
+						.addComponent(lblMainCharAge)
+						.addComponent(lblMainCharJob)
+						.addComponent(lblMainCharStatus))
+					.addGap(77)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblChar2Name)
+						.addComponent(lblChar2Age)
+						.addComponent(lblChar2Job)
+						.addComponent(lblChar2Status))
+					.addGap(79)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblChar3Name)
+						.addComponent(lblCha3Age)
+						.addComponent(lblChar3Job)
+						.addComponent(lblChar3Status))
+					.addGap(84)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblChar4Name)
+						.addComponent(lblChar4Age)
+						.addComponent(lblChar4Job)
+						.addComponent(lblChar4Status))
+					.addPreferredGap(ComponentPlacement.RELATED, 35, Short.MAX_VALUE)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblPace)
+						.addComponent(lblRations, GroupLayout.PREFERRED_SIZE, 19, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblCurrentPace)
+						.addComponent(lblCurrentRations))
+					.addGap(11))
 		);
 		frame.getContentPane().setLayout(groupLayout);
 	}

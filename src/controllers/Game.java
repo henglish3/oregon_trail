234b0961070947de46a04ae6b8af7c961982edd3
Stewart Hall <thestew42@gatech.edu>
2011-10-04 22:39:54 -0400

    Adding a shell for the Game class

---
+++ src/controllers/Game.java
@@ -0,0 +1,17 @@
+ /**
+ * Main controller object
+ * 
+ * @author S.H.L.A.T.
+ * @version 0.1 10/04/11
+ */
+
+public class Game implements ActionListener {
+    public Game() {
+        //TODO: Implement constructor
+    }
+
+    public void actionPerformed(ActionEvent e) { 
+        //code that reacts to the action 
+    }
+
+}

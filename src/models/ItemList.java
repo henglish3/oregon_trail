a3c9e3219fb72911ea7a5d2e4063e1fb00a701a7
Tomer Elmalem <telmalem@gmail.com>
2011-10-08 15:36:02 -0400

    Importing all of the necessary packes to parse XML files in ItemList. ortra-10

--- src/models/ItemList.java
+++ src/models/ItemList.java
@@ -7,6 +7,21 @@
 
 package edu.gatech.cs2340.shlat.models;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.Attribute;
+import javax.xml.stream.events.EndElement;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+
 public class ItemList {
   private int id;
   private String name;

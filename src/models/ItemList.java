1d55b9dfd855e6264555daa78570190583365cec
Tomer Elmalem <telmalem@gmail.com>
2011-10-08 13:11:57 -0400

    Created the ItemList model (ORTRA-10).

---
+++ src/models/ItemList.java
@@ -0,0 +1,14 @@
+/**
+ * Class that parses a list of items and turns them into item objects
+ *
+ * @author SHLAT
+ * @version 0.1 10/08/11
+ */
+
+package edu.gatech.cs2340.shlat.models;
+
+public class ItemList {
+  private int id;
+  private String name;
+  private String description;
+}

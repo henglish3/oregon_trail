f6277dd6e9019eeb2db1f0629ba3195d4e4817dc
Lucas Pessoa <LPessoa3@gatech.edu>
2011-10-09 02:25:30 -0400

    Made some progress on wagon class. inventory object commented out.

--- src/models/Wagon.java
+++ src/models/Wagon.java
@@ -1,2 +1,35 @@
 public class Wagon {
-}
+    
+    //private Inventory inventory = new Inventory();
+    private boolean wheel;
+    private boolean axle;
+    private boolean tongue;
+    private boolean yoke;
+    
+    public Wagon() {
+        wheel = true;
+        axle = true;
+        tongue = true;
+        yoke = false; //player needs to buy it
+    } //ends Wagon constructor.
+    
+    //alwayr run this method when player clicks to move.
+    public void checkWagon() {
+        if(wheel == true && axle == true && tongue == true && yoke == true) {
+            //run method that allows wagon to move. 
+            //we could also add weight check, food check and ability to drop/sell items anywhere.
+        }
+        else if(wheel == false) {
+            System.out.println("A wheel on your Wagon is broken and needs repairs.");
+        }
+        else if(axle == false) {
+            System.out.println("An axle on your Wagon is broken and needs repairs.");
+        }
+        else if(tongue == false) {
+            System.out.println("The tongue on your Wagon is broken and needs repairs.");
+        }
+        else {
+            System.out.println("You need a yoke to move the wagon.");
+        }   
+    } //ends checkWagon method.
+} //ends Wagon class.

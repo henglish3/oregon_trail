352197eeb8b2f1dd5358f83eff48abefecd9993f
Tomer Elmalem <telmalem@gmail.com>
2011-10-09 13:22:20 -0400

    Implemented an xml parser for items.xml but it's untested and doesn't compile at the moment because it's dependent on the Item class which doesn't exist test. ortra-10

--- src/models/ItemList.java
+++ src/models/ItemList.java
@@ -23,7 +23,64 @@ import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
 
 public class ItemList {
+  static final String ITEM = "item";
   static final String ID = "id";
   static final String NAME = "name";
   static final String DESC = "desc";
+
+  public List<Item> readConfig(String configFile) {
+    List<Item> items = new ArrayList<Item>();
+    try {
+      XMLInputFactory inputFactory = XMLInputFactory.newInstance();
+
+      InputStream in = new FileInputStream(configFile);
+      XMLEventReader eventReader = inputFactory.createXMLEventReader(in);
+
+      Item item = null;
+
+      while (eventReader.hasNext()) {
+        XMLEvent event = eventReader.nextEvent();
+
+        if (event.isStartElement()) {
+          StartElement startElement = event.asStartElement();
+
+          if (startElement.getName().getLocalPart() == (ITEM)) {
+            item = new Item(); 
+          }
+
+          if (event.isStartElement()) {
+            if (event.asStartElement().getName().getLocalPart().equals(ID)) {
+              event = eventReader.nextEvent();
+              item.setId(event.asCharacters().getData());
+              continue;
+            }
+          }
+
+          if (event.asStartElement().getName().getLocalPart().equals(NAME)) {
+            event = eventReader.nextEvent();
+            item.setName(event.asCharacters().getData());
+            continue;
+          }
+          
+          if (event.asStartElement().getName().getLocalPart().equals(DESC)) {
+            event = eventReader.nextEvent();
+            item.setDescription(event.asCharacters().getData());
+            continue;
+          }
+        }
+
+        if (event.isEndElement()) {
+          EndElement endElement = event.asEndElement();
+          if (endElement.getName().getLocalPart() == (ITEM)) {
+            items.add(item);
+          }
+        }
+      }
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (XMLStreamException e) {
+      e.printStackTrace();
+    }
+    return items;
+  }
 }

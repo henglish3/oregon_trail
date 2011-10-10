f766e246c897fff0096d6531be6a7bf2f1510931
Tomer Elmalem <telmalem@gmail.com>
2011-10-10 13:41:05 -0400

    Added inventory variable and basic constructor to the Store model.

---
+++ src/models/Store.java
@@ -0,0 +1,10 @@
+package edu.gatech.cs2340.shlat.models;
+
+public class Store {
+  private Inventory inventory;
+
+  // Constructor for a store instance
+  public Store() {
+    inventory = new Inventory();
+  }
+}

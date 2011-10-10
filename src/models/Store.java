94e5fdaae81e1c7340fa09acf5fb02b7c4b24006
Tomer Elmalem <telmalem@gmail.com>
2011-10-10 14:26:26 -0400

    Initial purchase logic added: loops through the inventory to find the item.

--- src/models/Store.java
+++ src/models/Store.java
@@ -7,4 +7,16 @@ public class Store {
   public Store() {
     inventory = new Inventory();
   }
+
+  public void purchase(int item_id, Player player);
+    Item item = null;
+    List inventoryList = inventory.getList();
+    for (Item itemTmp : inventoryList) {
+      int id = item.getId();
+      if (id == item_id) {
+        item = itemTmp;
+        break;
+      }
+    }
+  }
 }

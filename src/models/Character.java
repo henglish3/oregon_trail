bc04e9efe8a243df2cf4fb9e643c4f42783a075e
Lucas Pessoa <LPessoa3@gatech.edu>
2011-10-10 12:57:31 -0400

    Java Doc all files in the models folder.

--- src/models/Character.java
+++ src/models/Character.java
@@ -5,7 +5,7 @@
  * @version 0.1 10/03/11
  */
  
- package edu.gatech.cs2340.shlat.models;
+package edu.gatech.cs2340.shlat.models;
  
 public class Character {
 
@@ -21,89 +21,104 @@ public class Character {
     private int sex; //female = 0, male = 1
     private String name;
  
+    /**
+     * Constructor that make instantiates the character's variables.
+     * 
+     * @param age The character's age.
+     * @param sex The character's sex.
+     * @param name The character's name.
+     */    
     public Character(int age, int sex, String name) {
         this.age = age;
         this.sex = sex;
         this.name = name;
         status = Status.NORMAL;
-    }
+    } //ends Character constructor.
     
-    public Character(int age, int sex, String name, Status status) {
-    	this.age = age;
-    	this.sex = sex;
-        this.name = name;
-    	this.status = status;
-    }
     /**
-    * Method that gets the character's status.
-    * 
-    * @return The status of the character.
-    */
+     * Constructor that make instantiates the character's variables.
+     * 
+     * @param age The character's age.
+     * @param sex The character's sex.
+     * @param name The character's name.
+     * @param staus The character's status.
+     */ 
+    public Character(int age, int sex, String name, Status status) {
+        this.age = age;
+        this.sex = sex;
+        this.name = name;
+        this.status = status;
+    } //ends Character constructor.
+    
+    /**
+     * Method that gets the character's status.
+     * 
+     * @return The status of the character.
+     */
     public Status getStatus() {
         return status;
     } //ends getStatus method.
  
     /**
-    * Method that sets the character's status.
-    * 
-    * @param status The status the character will have. 
-    */
+     * Method that sets the character's status.
+     * 
+     * @param status The status the character will have. 
+     */
     public void setStatus(Status status) {
         this.status = status;
     } //ends setStatus method.
     
     /**
-    * Method that gets the character's name.
-    * 
-    * @return The name of the character.
-    */
+     * Method that gets the character's name.
+     * 
+     * @return The name of the character.
+     */
     public String getName() {
         return name;
     } //ends getName method.
  
     /**
-    * Method that sets the character's name.
-    * 
-    * @param name The name the character will have. 
-    */
+     * Method that sets the character's name.
+     * 
+     * @param name The name the character will have. 
+     */
     public void setName(String name) {
         this.name = name;
     } //ends setName method.
   
     /**
-    * Method that gets the character's age.
-    * 
-    * @return The age of the character.
-    */
+     * Method that gets the character's age.
+     * 
+     * @return The age of the character.
+     */
     public int getAge() {
         return age;
     } //ends getAge method.
   
     /**
-    * Method that sets the character's age.
-    * 
-    * @param age The age the character will have. 
-    */
+     * Method that sets the character's age.
+     * 
+     * @param age The age the character will have. 
+     */
     public void setAge(int age) {
         this.age = age;
     } //ends setAge method.
   
     /**
-    * Method getSex gets the character's sex.
-    * 
-    * @return The sex of the character.
-    */
+     * Method getSex gets the character's sex.
+     * 
+     * @return The sex of the character.
+     */
     public int getSex() {
         return sex;
     } //ends getSex method.
   
     /**
-    * Method that sets the character's sex.
-    * 
-    * @param sex The sex the character will have. 
-    */
+     * Method that sets the character's sex.
+     * 
+     * @param sex The sex the character will have. 
+     */
     public void setSex(int sex) {
         this.sex = sex;
     } //ends setSex method.
-  
 } //ends Character class.
--- src/models/Player.java
+++ src/models/Player.java
@@ -8,13 +8,23 @@
 package edu.gatech.cs2340.shlat.models;
  
 public class Player extends Character {
+    
+    /**
+     * Constructor that make instantiates the player's variables.
+     * 
+     * @param age The player's age.
+     * @param sex The player's sex.
+     * @param name The player's name.
+     * @param job The player's job.
+     */  
     public Player(int age, int sex, String name, Job job) {
         super(age, sex, name, Status.IMMORTAL);
         this.job = job;
-    }
+    } //ends Player constructor.
+    
     /**
-    * Enumerated type which identifies the character's job.
-    */
+     * Enumerated type which identifies the character's job.
+     */
     public enum Job {
         BANKER, FARMER, CARPENTER;
     } //ends Job enum.
@@ -22,10 +32,10 @@ public class Player extends Character {
     private Job job;
 
     /**
-    * Method that gets the job of the character.
-    * 
-    * @return The job of the character.
-    */
+     * Method that gets the job of the character.
+     * 
+     * @return The job of the character.
+     */
     public Job getJob() {
         return job;
     } //ends getJob method.
@@ -39,7 +49,12 @@ public class Player extends Character {
         this.job = job;
     } //ends setJob method.
 
+    /**
+     * Method that returns a string value of the player's job.
+     * 
+     * @return The string value of the player's job.
+     */    
     public String jobString() {
       return job.toString();
-    }
+    } //ends jobString.
 } //ends Player class.

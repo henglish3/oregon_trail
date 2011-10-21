d6fed262bcf8f9c709fb1bc37481f80574542d58
Lucas Pessoa <LPessoa3@gatech.edu>
2011-10-20 20:48:58 -0400

    Changed Party.java constructor and wrote get/setCharacterStatus methods.

--- src/models/Party.java
+++ src/models/Party.java
@@ -8,53 +8,25 @@
 package edu.gatech.cs2340.shlat.models;
 import java.awt.event.*;
 import java.awt.EventQueue;
-
+import edu.gatech.cs2340.shlat.models.Character;
 public class Party {
     private Character[] characters;
     
-    /*
-     * Constructor that assigns the size of the party and adds the characters to the party. 
+    /**
+     * Constructor that assigns the four characters to the party.
      * The player is not added to the party.
      * 
-     * @param charAmount The amount of characters that will be in the party.
      * @param char0 The first character in the party.
      * @param char1 The second character in the party.
      * @param char2 The thrid character in the party.
      * @param char3 The fourth character in the party.
-     *
-    public Party(int charAmount, Character char0, Character char1, Character char2, Character char3) {
-        characters = new Character[charAmount];
-        int i = 0;
-        if(i < characters.length) {
-            characters[i] = char0;
-            i++;
-        }
-        if(i < characters.length) {
-            characters[i] = char1;
-            i++;
-        }
-        if(i < characters.length) {
-            characters[i] = char2;
-            i++;
-        }
-        if(i < characters.length) {
-            characters[i] = char3;
-            i++;
-        }
-    } //ends Party constructor.
-    */
-    
-    /**
-     * Constructor that assigns the size of the party and adds the characters to the party. 
-     * The player is not added to the party.
-     * 
-     * @param characters The characters that will be added to the characters array.
      */
-    public Party(Character ... characters) {
-        this.characters = new Character[characters.length];
-        for (int i = 0; i < characters.length; i++) {
-            this.characters[i] = characters[i];
-        }
+    public Party(Character char0, Character char1, Character char2, Character char3) {
+        characters = new Character[4];
+        characters[0] = char0;
+        characters[1] = char1;
+        characters[2] = char2;
+        characters[3] = char3;
     } //ends Party constructor.
     
     /**
@@ -74,14 +46,33 @@ public class Party {
     public void setCharacters(Character characters[]) {
         this.characters = characters;
     } //ends setCharacters method.
- 
+    
+    /**
+     * Method that gets a certain character's status in the party.
+     * 
+     * @return The character's status.
+     */
+    public Status getCharacterStatus(int charNum) {
+        return characters[charNum].getStatus();
+    } //ends getCharacterStatus method.
+
+    /**
+     * Method that sets a certain character's status in the party.
+     * 
+     * @param charNum The character who's status will change.
+     * @param statChange The status the character will now have.
+     */
+    public void setCharacterStatus(int charNum, Status statChange) {
+        characters[charNum].setStatus(statChange);
+    } //ends setCharacterStatus method.
+    
     /**
      * Listener class implements ActionListener.
      */
     private class Listener implements ActionListener {
         
         /**
-         * actionPerformed method...
+         * Method...
          * 
          * @param e The event being taken by the method.
          */

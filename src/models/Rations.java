46b3c23f349af741cc2129966e58de0834e7cf38
Tomer Elmalem <telmalem@gmail.com>
2011-10-27 13:56:08 -0400

    Changed the rations class to use ints similarly to the pace class.

--- src/models/Rations.java
+++ src/models/Rations.java
@@ -7,38 +7,29 @@
 package edu.gatech.cs2340.shlat.models;
 
 public class Rations {
-  private String ration;
+  private int ration;
 
   /**
    * Method creates a Rations object with a selected rations description
-   * @param rationsSelection The rations of the player
+   * @param rations The rations of the player
    */
-  public Rations(int rationsSelection){
-    if(rationsSelection == 0)//0 == None
-      ration = "None";
-    else if(rationsSelection == 1) //1 == Bare-bones
-      ration = "Bare-bones";
-    else if(rationsSelection == 2) //2 == Meager
-      ration = "Meager";
-    else if(rationsSelection == 3)//3 == Normal
-      ration = "Normal";
-    else if(rationsSelection == 4)//4 == Well fed
-      ration = "Well fed";
+  public Rations(int rations){
+    this.ration = rations;
   }
 
   /**
    * Method returns this player's ration description
    * @return ration This player's ration description
    */
-  public String getRation(){
+  public int getRation(){
     return ration;
   }
 
   /**
    * Method sets a new description for this player's ration
-   * @param ration The new description for this player's ration
+   * @param ration The new ration for the player
    */
-  public void setRation(String ration){
+  public void setRation(int ration){
     this.ration = ration;
   }
 }

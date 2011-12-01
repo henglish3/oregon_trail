8146d3c93d4ee99a9693e802812525bfc0172d62
Tomer Elmalem <telmalem@gmail.com>
2011-12-01 18:39:48 -0500

    Create the store tests file.

---
+++ src/tests/StoreTests.java
@@ -0,0 +1,21 @@
+package edu.gatech.cs2340.shlat.tests;
+
+import junit.frameworks.TestCase;
+
+public class StoreTest extends TestCase {
+  private Wagon w;
+  private Player p;
+  private int money;
+
+  public void testAmount() {
+    
+  }
+
+  public void testCost() {
+
+  }
+
+  public void testWeight() {
+
+  }
+}

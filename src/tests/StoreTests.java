e9a5a78631cbff76ada22f8400443b97d99bf71d
Tomer Elmalem <telmalem@gmail.com>
2011-12-01 19:29:51 -0500

    Initialize the fixtures.

--- src/tests/StoreTests.java
+++ src/tests/StoreTests.java
@@ -1,14 +1,22 @@
 package edu.gatech.cs2340.shlat.tests;
 
+import org.junit.*;
 import org.junit.Test;
 import org.junit.Assert;
 import edu.gatech.cs2340.shlat.models.*;
 
 public class StoreTests {
-  private Wagon w = new Wagon();
-  private Player p = new Player();
-  private Item i = new Item();
-  private Store s = new Store();
+  private Wagon w;
+  private Player p;
+  private Item i;
+  private Store s;
+
+  @Before public void setUp() {
+    w = new Wagon();
+    p = new Player();
+    i = new Item();
+    s = new Store();
+  }
 
   @Test public void testAmount() {
     String expected = "Purchased.";

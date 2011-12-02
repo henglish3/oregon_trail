98b7a8aa17d0e1d7c0c2aa3ad1a42968d18eeed6
Tomer Elmalem <telmalem@gmail.com>
2011-12-01 19:34:59 -0500

    Remove player and item from the fixture since that will change.

--- src/tests/StoreTests.java
+++ src/tests/StoreTests.java
@@ -7,18 +7,18 @@ import edu.gatech.cs2340.shlat.models.*;
 
 public class StoreTests {
   private Wagon w;
-  private Player p;
-  private Item i;
   private Store s;
 
   @Before public void setUp() {
     w = new Wagon();
-    p = new Player();
-    i = new Item();
     s = new Store();
   }
 
   @Test public void testAmount() {
+    Player p = new Player();
+    p.initializeInventory();
+    Item i = new Item("Pimp Wagon", 1, 2500, 100);
+
     String expected = "Purchased.";
     String result = s.purchase(p, w, i, 100);
     Assert.assertTrue(expected.equals(result));

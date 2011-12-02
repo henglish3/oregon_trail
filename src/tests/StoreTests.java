50993b9f6188facd0457d2869703e2c1718eb734
Tomer Elmalem <telmalem@gmail.com>
2011-12-01 19:07:06 -0500

    Game compiles again. Add the junit jar file to the repository.

--- src/tests/StoreTests.java
+++ src/tests/StoreTests.java
@@ -1,21 +1,25 @@
 package edu.gatech.cs2340.shlat.tests;
 
-import junit.frameworks.TestCase;
+import org.junit.*;
+import edu.gatech.cs2340.shlat.models.*;
 
-public class StoreTest extends TestCase {
+public class StoreTests {
   private Wagon w = new Wagon();
   private Player p = new Player();
   private Item i = new Item();
   private Store s = new Store();
 
+  @Test 
   public void testAmount() {
     
   }
 
+  @Test 
   public void testCost() {
 
   }
-
+  
+  @Test
   public void testWeight() {
 
   }
